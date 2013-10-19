907b296c32a79582f4a458c139d6f32cf6c9c9a3
Chris Evans <chris.evans93@gmail.com>
2013-10-19 02:24:17 -0400

    Basic landing page @test.html

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,18 @@
+$(function () {
 
+	if (!("R" in window)) {
+		alert('Something has gone wrong!');
+		return;
+	}
+
+	R.ready(function () {
+
+		var player = new metronomik.player("player", R.player);
+
+		$(".m-playToggle").one("click", function () {
+			R.player.queue.add("a171827");
+		});
+
+	});
+
+});
