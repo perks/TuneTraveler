7154ce8d8fe7eee07c207f19487dae31a9d4b659
Chris Evans <chris.evans93@gmail.com>
2013-10-19 00:07:14 -0400

    Moved to templates directory

--- app.py
+++ app.py
@@ -11,21 +11,22 @@ app.config.update(
     DEBUG = True,
 )
 
+
 #----------------------------------------
 # controllers
 #----------------------------------------
 
-@app.route("/")
-def index():
-		return render_template('index.html')
-
 @app.route('/favicon.ico')
 def favicon():
-		return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
 
 @app.errorhandler(404)
-def page_note_found(e):
-		return render_template('404.html'), 404
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route("/")
+def index():
+    return render_template('index.html')
 
 
 #----------------------------------------
