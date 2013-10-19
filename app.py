494849b66b96f66c90822b79b229dd47b33dca7d
Chris Evans <chris.evans93@gmail.com>
2013-10-19 00:04:37 -0400

    Added basehtml and bootstrap template

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 #----------------------------------------
 # initialization
@@ -16,8 +16,17 @@ app.config.update(
 #----------------------------------------
 
 @app.route("/")
-def hello():
-    return "Hello from Python!"
+def index():
+		return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+		return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_note_found(e):
+		return render_template('404.html'), 404
+
 
 #----------------------------------------
 # launch
