825b071ac812398e03d7c5d67b114c206ef7a4e2
Chris Evans <chris.evans93@gmail.com>
2013-10-19 06:34:22 -0400

    Still rdio ids...

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,11 @@
 var Tune = {};
+var rdioIDs = [];
+
 
 $(function () {
 
 
-	function getArtistData(data){
-		Tune.response = data.response;
-		Tune.artists = data.response.artists;
-	}
+
 
 	if (!("R" in window)) {
 		alert('Something has gone wrong!');
@@ -24,6 +23,14 @@ $(function () {
 	});
 
 
+});
+
+
+
+function callEchoNext(b, a, l) {
+	var before = b;
+	var after = a;
+	var location = l;
 	$.ajax({
 		url: "http://developer.echonest.com/api/v4/artist/search",
 		dataType: "jsonp",
@@ -31,15 +38,30 @@ $(function () {
 		data: {
 			api_key: "DK23QQCJUWLKC1HPL",
 			format: "jsonp",
-			artist_start_year_after: "1969",
-			artist_start_year_before: "1990",
-			artist_location: "london",
+			artist_start_year_after: after,
+			artist_start_year_before: before,
+			artist_location: location,
 			bucket: ["id:rdio-us-streaming", "years_active", "artist_location"],
 		},
 		success: getArtistData
 	});
+}
 
-});
+
+function getArtistData(data){
+	Tune.response = data.response;
+	Tune.artists = data.response.artists;
+	var regEx = /[^:]+$/;
+	for(var artist in Tune.artists) {
+		var name = Tune.artists[artist].name;
+		var id = Tune.artists[artist].foreign_ids[0].foreign_id.match(regEx);
+		rdioIDs.push({name : id });
+		console.log(name, id);
+	}
+}
+
+
+
 
 
 
